eb2d19ce79a36a2e3bcf10e8f45c3596977757ce
vraelML <lilian.macrez@edu.esiee.fr>
2024-12-04 14:12:06 +0100

    ajout de commentaires

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.express as px
 
-file_path = 'Data/raw/temperature-quotidienne-departementale.csv'
+# Charger le fichier CSV
+file_path = 'Data/raw/temperature-quotidienne-departementale.csv' 
 temperature_df = pd.read_csv(file_path, delimiter=';')
 
+# Convertir la colonne 'Date' en format datetime pour une meilleure gestion des dates
 temperature_df['Date'] = pd.to_datetime(temperature_df['Date'])
 
+# Initialiser l'application Dash
 app = dash.Dash(__name__)
 
+# Mise en page de l'applicaation
 app.layout = html.Div([
     html.H1("Évolution des Températures par Département", style={'textAlign': 'center'}),
     
+    # Menu déroulant pour sélectionner le département
     dcc.Dropdown(
         id='departement-dropdown',
         options=[{'label': dept, 'value': dept} for dept in sorted(temperature_df['Département'].unique())],
@@ -21,19 +26,24 @@ app.layout = html.Div([
         placeholder="Sélectionnez un département"
     ),
     
+    # Graphique interactif
     dcc.Graph(id='temperature-histogram')
 ])
 
+# Mise à jour du graphique en fonction du département sélectionné
 @app.callback(
     Output('temperature-histogram', 'figure'),
     [Input('departement-dropdown', 'value')]
 )
 def update_histogram(selected_departement):
+    # Filtrer les données en fonction du département sélectionné
     filtered_df = temperature_df[temperature_df['Département'] == selected_departement]
     
+    # Calculer la température moyenne par date pour le département sélectionné
     daily_avg_temp = filtered_df.groupby('Date')['TMoy (°C)'].mean().reset_index()
     
-    fig = px.line(
+    # Créer le graphique de l'évolution des températures
+    graph = px.line(
         daily_avg_temp,
         x='Date',
         y='TMoy (°C)',
@@ -42,13 +52,14 @@ def update_histogram(selected_departement):
         line_shape='linear'
     )
 
-    fig.update_layout(
+    graph.update_layout(
         xaxis_title='Date',
         yaxis_title='Température Moyenne (°C)',
         height=600
     )
 
-    return fig
+    return graph
 
+# Exécuter l'application
 if __name__ == '__main__':
     app.run_server(debug=True, port=8060)
