cd6c7d568d72b85eeed6fb83ca7d031d9ed3cea8
KOrmazd <juliensafinia@gmail.com>
2025-01-17 22:50:00 +0100

    get data correct

--- Data/raw/get_data.py
+++ Data/raw/get_data.py
@@ -1,20 +1,20 @@
 import requests
 
-# URL du fichier CSV
-csv_url = "https://files.data.gouv.fr/dd0df06a-85f2-4621-8b8b-5a3fe195bcd7/20230101-20231231.csv"
+# URL directe du fichier CSV
+csv_url = "https://www.data.gouv.fr/fr/datasets/r/dd0df06a-85f2-4621-8b8b-5a3fe195bcd7"
 
-# Nom du fichier à sauvegarder localement
-output_file = "/raw/temperature_quotidienne.csv"
+# Nom du fichier local
+output_file = "temperature_quotidienne.csv"
 
 try:
-    # Récupérer les données depuis l'URL
+    print("Téléchargement du fichier...")
     response = requests.get(csv_url)
     response.raise_for_status()  # Vérifie les erreurs HTTP
+    print("Téléchargement réussi.")
     
-    # Sauvegarder le fichier localement
+    # Sauvegarder le fichier
     with open(output_file, "wb") as file:
         file.write(response.content)
-    
     print(f"Fichier téléchargé et sauvegardé sous le nom '{output_file}'")
 except requests.exceptions.RequestException as e:
     print(f"Erreur lors du téléchargement : {e}")
